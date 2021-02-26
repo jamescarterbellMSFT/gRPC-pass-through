d39ad9a8c1f7508ce27faf4e375143570512634b
James Bell <jamesbell@microsoft.com>
2021-02-26 09:36:24 -0800

    Trying warp

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-#![feature(proc_macro_hygiene, decl_macro, async_closure)]
-
-#[macro_use] extern crate rocket;
-
 use std::future::Future;
-use rocket::{Route, Request, Data, State, handler::{Outcome, Handler}, data::{FromData}, http::Method};
-use rocket_contrib::json::Json;
 use corp::vault_client::VaultClient;
 use tonic::transport::channel::Channel;
 use crossbeam::channel::{bounded, Sender, Receiver};
 use serde::Serialize;
 use std::pin::Pin;
+use warp::{Filter, Rejection};
 
 
 pub mod corp{
@@ -19,18 +14,15 @@ pub mod corp{
  
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>  {
-    let route = GrpcHandlerBuilder::<VaultClient<Channel>>::build_route(Method::Put, "/deposit", |c, m| Box::pin(VaultClient::deposit(&mut c, m)));
-    rocket::ignite()
-        .mount("/", routes![withdraw])
-        //.mount("/", vec![(builder)])
-        .manage(
-            ClientPool::build_n_with_async(
-                24, async || VaultClient::connect("https://localhost:8081").await.unwrap()
-            ).await
-        )
-        .launch()
-        .await?;
-
+    //let f = FilterBuilder::build(warp::put(), "/deposit", VaultClient::deposit);
+    let pool = ClientPool::build_n_with_async(12, || async {VaultClient::connect("127.0.0.1:8081").await.unwrap()}).await;
+    let f = warp::put()
+                .and(warp::path("/deposit"))
+                .and(warp::body::json())
+                .and_then(|q: corp::DepositRequest| async {
+                    let mut client = pool.get_client_async().await;
+                    Ok(client.deposit(q).await.unwrap())
+                });
     Ok(())
 }
 
@@ -39,7 +31,8 @@ pub struct ClientPool<T>{
     receiver: Receiver<T>,
 }
 
-impl<T> ClientPool<T>{
+impl<T> ClientPool<T>
+    where T: 'static + Send{
     pub fn build_n_with(n: usize, builder: fn() -> T) -> Self{
         let (sender, receiver) = bounded(n);
         
@@ -77,6 +70,16 @@ impl<T> ClientPool<T>{
             client: Some(self.receiver.recv().unwrap())
         }
     }
+
+    pub async fn get_client_async<'a>(&'a self) -> ClientGuard<'a, T>{
+        let mut recv = self.receiver.clone();
+        ClientGuard{
+            source: self,
+            client: tokio::task::spawn_blocking(move ||{
+                Some(recv.recv().unwrap())
+            }).await.unwrap(),
+        }
+    }
 }
 
 pub struct ClientGuard<'a, T>{
@@ -106,93 +109,15 @@ impl<'a, T> std::ops::DerefMut for ClientGuard<'a, T>{
     }
 }
 
-#[put("/deposit", data = "<req>")]
-async fn deposit(req: Json<corp::DepositRequest>, pool: State<'_, ClientPool<VaultClient<Channel>>>) -> Result<Json<corp::AccountReply>, String>{
-    let mut grpc_client = pool.get_client();
-    match grpc_client.deposit(req.into_inner()).await{
-        Ok(res) => Ok(Json(res.into_inner())),
-        Err(e) => Err(e.to_string()),
+/*
+struct FilterBuilder;
+
+impl FilterBuilder{
+    fn build<C, M, R>(method: impl Filter, path: impl AsRef<str>, grpc_fn: impl Fn(&mut C, M) -> Future<Output = R>) -> warp::AndThen{
+        let f = method
+            .and(warp::path(path))
+            .and_then();
+        Box::new(f)
     }
 }
-
-#[put("/withdraw", data = "<req>")]
-async fn withdraw(req: Json<corp::WithdrawRequest>, pool: State<'_, ClientPool<VaultClient<Channel>>>) -> Result<Json<corp::AccountReply>, String>{
-    let mut grpc_client = pool.get_client();
-    match VaultClient::withdraw(&mut grpc_client, req.into_inner()).await{
-        Ok(res) => Ok(Json(res.into_inner())),
-        Err(e) => Err(e.to_string()),
-    }
-}
-
-
-struct GrpcHandlerBuilder<C>{
-    c: std::marker::PhantomData<C>,
-}
-
-impl<C> GrpcHandlerBuilder<C>
-    where C: 'static + Sync + Send{
-
-    fn build_handler<F, M, R>(grpc_fn: F) -> GrpcHandler<C, M, R, F>
-        where F: 'static + Send + Sync + Fn(&mut C, M) -> Pin<Box<dyn Future<Output = Result<tonic::Response<R>, tonic::Status>> + Sync + Send + 'static>> + Copy,
-            M: 'static + Sync + Send + FromData,
-            R: 'static + Sync + Send + Serialize,{
-        GrpcHandler{
-            f: Box::pin(grpc_fn),
-            c: std::marker::PhantomData,
-            m: std::marker::PhantomData,
-            r: std::marker::PhantomData,
-        }
-    }
-
-    fn build_route<S, F, M, R>(method: Method, path: S, grpc_fn: F) -> Route
-        where F: 'static + Send + Sync + Fn(&mut C, M) -> Pin<Box<dyn Future<Output = Result<tonic::Response<R>, tonic::Status>> + Sync + Send + 'static>> + Copy,
-            M: 'static + Sync + Send + FromData,
-            R: 'static + Sync + Send + Serialize,
-            S: AsRef<str>{
-        let handler = Self::build_handler::<F, M, R>(grpc_fn);
-        Route::new(method, path, handler)
-    }
-}
-
-
-struct GrpcHandler<C, M, R, F>
-    where F: Copy{
-    f: Pin<Box<F>>,
-    c: std::marker::PhantomData<C>,
-    m: std::marker::PhantomData<M>,
-    r: std::marker::PhantomData<R>,
-}
-
-impl<C, M, R, F> Clone for GrpcHandler<C, M, R, F>
-    where F: Copy{
-
-    fn clone(&self) -> Self{
-        Self{
-            f: Box::pin(*self.f),
-            c: std::marker::PhantomData,
-            m: std::marker::PhantomData,
-            r: std::marker::PhantomData
-        }
-    }
-}
-
-
-#[rocket::async_trait]
-impl<C, M, R, F> Handler for GrpcHandler<C, M, R, F>
-    where F: 'static + Send + Sync + Fn(&mut C, M) -> Pin<Box<dyn Future<Output = Result<tonic::Response<R>, tonic::Status>> + Sync + Send + 'static>> + Copy,
-          M: 'static + Sync + Send + FromData,
-          R: 'static + Sync + Send + Serialize,
-          C: 'static + Sync + Send,{
-    async fn handle<'r, 's: 'r>(&'s self, req: &'r Request<'_>, data: Data) -> Outcome<'r>{
-        let pool = req.guard::<State<'_, ClientPool<C>>>().await.unwrap();
-        let mut grpc_client = pool.get_client();
-        let message = M::from_data(req, data).await.unwrap();
-        match (self.f)(&mut grpc_client, message).await{
-            Ok(res) => Outcome::from(req, Json(res.into_inner())),
-            Err(e) => Outcome::Failure(rocket::http::Status::new(
-                404,
-                "I don't know tbh"
-            )),
-        }
-    }
-}
+*/
